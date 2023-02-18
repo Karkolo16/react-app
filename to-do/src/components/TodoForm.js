0d4f55af13687703e70301cdf272f5d2c6a27d20
Karolina <ka.kolomyjec@gmail.com>
2023-02-18 12:07:03 +0100

    add TodoForm

---
+++ to-do/src/components/TodoForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+function TodoForm() {
+    const[input, setInput] = useState('')
+
+  return (
+    <form className="todo-form">
+
+    <input  type="text"
+            placeholder="Add a todo" 
+            value={input} 
+            name="text" 
+            className="todo-input">
+    </input>
+
+    <button className="todo-button">Add activity</button>
+    </form>
+  )
+}
+
+export default TodoForm
