ac76997d9720c2278cc783ba0c5d3f4d483e31ba
Karolina <ka.kolomyjec@gmail.com>
2023-02-18 12:21:55 +0100

    add function that not refresh the page and function that make possibility to write in input

--- to-do/src/components/TodoForm.js
+++ to-do/src/components/TodoForm.js
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
 
 
-function TodoForm() {
-    const[input, setInput] = useState('')
+function TodoForm(props) {
+    const[input, setInput] = useState('');
+
+//this function make possibility to write text in input
+    const handleChange = e  => {
+        setInput(e.target.value)
+    }
+//
+//this make the page not refresh when I click button
+
+const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+        id:Math.floor(Math.random() * 10000),
+        text: input
+    });
+}
+
+//
 
   return (
-    <form className="todo-form">
-
-    <input  type="text"
-            placeholder="Add a todo" 
-            value={input} 
-            name="text" 
-            className="todo-input">
-    </input>
-
-    <button className="todo-button">Add activity</button>
+    <form className="todo-form" onSubmit={handleSubmit}>
+            <input  type="text"
+                placeholder="Add a todo" 
+                value={input} 
+                name="text" 
+                className="todo-input"
+                onChange={handleChange}>
+            </input>
+        <button className="todo-button">Add activity</button>
     </form>
   )
 }
