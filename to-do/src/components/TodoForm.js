666a5afb61d7583828c76d7cfbdc0a35d7320376
Karolina <ka.kolomyjec@gmail.com>
2023-02-25 11:15:42 +0100

    finnished code to make app works

--- to-do/src/components/TodoForm.js
+++ to-do/src/components/TodoForm.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 
 function TodoForm(props) {
     const[input, setInput] = useState('');
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
 
 //this function make possibility to write text in input
     const handleChange = e  => {
@@ -10,7 +16,6 @@ function TodoForm(props) {
     };
 //
 //this make the page not refresh when I click button
-
 const handleSubmit = e => {
     e.preventDefault();
 
@@ -31,7 +36,9 @@ const handleSubmit = e => {
                 value={input} 
                 name="text" 
                 className="todo-input"
-                onChange={handleChange}>
+                onChange={handleChange}
+                ref={inputRef}
+                >
             </input>
         <button className="todo-button">Add activity</button>
     </form>
